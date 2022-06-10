38ea00abdffbcde289d60cf6740be91606b34cef
kiuen123 <kien121999121999@gmail.com>
2022-06-10 18:02:38 +0700

    update

--- wecodelab_server/index.js
+++ wecodelab_server/index.js
@@ -2,6 +2,20 @@ var express = require("express");
 var database = require("./db.js");
 var app = express();
 
+var currentdate = new Date();
+var datetime =
+    currentdate.getDate() +
+    "-" +
+    (currentdate.getMonth() + 1) +
+    "-" +
+    currentdate.getFullYear() +
+    "_" +
+    currentdate.getHours() +
+    "-" +
+    currentdate.getMinutes() +
+    "-" +
+    currentdate.getSeconds();
+
 app.get("/api/get", function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     database.get(function (data) {
@@ -12,7 +26,7 @@ app.get("/api/get", function (req, res) {
 
 app.get("/api/add", function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
-    database.add(req.query.w, req.query.per, req.query.date, function (data) {
+    database.add(req.query.w, req.query.per, datetime, function (data) {
         console.log(data);
         res.send(data);
     });
